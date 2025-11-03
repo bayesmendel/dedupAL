30656e6d7fd60a6d8bda195ba17df3eabddba422
M. S. Rosito <82741585+msrosito@users.noreply.github.com>
2025-11-03 12:35:16 -0500

    conf files

---
+++ src/config.py
@@ -0,0 +1,6 @@
+# src/config.py
+
+CANCER_NAMES = [
+    'Adrenal', 'Brain', 'Breast', 'Leukemia', 'Lung', 'Osteosarcoma',
+    'Phyllodes', 'Sarcoma', 'Soft tissue sarcoma', 'Colorectal', 'Pancreatic'
+]
