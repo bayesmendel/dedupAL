30978e652dbfe6acb47378e2abc419af65052941
M. S. Rosito <82741585+msrosito@users.noreply.github.com>
2025-11-04 16:56:46 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,3 +4,6 @@ CANCER_NAMES = [
     'Adrenal', 'Brain', 'Breast', 'Leukemia', 'Lung', 'Osteosarcoma',
     'Phyllodes', 'Sarcoma', 'Soft tissue sarcoma', 'Colorectal', 'Pancreatic'
 ]
+
+N_threshold = 3
+p_threshold = 0.85
